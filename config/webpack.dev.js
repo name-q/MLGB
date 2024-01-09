42e19cfb5cf20b90de74dd149c41937b03230b62
name-q <1620206666@qq.com>
2024-01-09 15:43:25 +0800

    handle css

---
+++ config/webpack.dev.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+    entry: './src/main.js',
+
+    output: {
+        path: undefined,
+        filename: 'static/js/[name].js',
+        chunkFilename: 'statuc/js/[name].chunk.js',
+        assetModuleFilename: 'static/media/[hash:10][ext][query]'
+    },
+
+    module: {
+        rules: [
+            // handle css
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+}
